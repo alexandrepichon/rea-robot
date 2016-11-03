c4c34baac77928fd0a489a68f8b9d72fae8e722b
alexandre pichon <alexandre.pichon.ap@gmail.com>
2016-11-03 17:26:49 +0100

    read input file

--- src/main/scala/ToyRobotSimulator.scala
+++ src/main/scala/ToyRobotSimulator.scala
@@ -1,8 +1,9 @@
+import scala.io.Source
 
 object ToyRobotSimulator extends App {
-  ???
+  apply(Source.fromFile(args(0)).getLines().toSeq:_*)
 
   def apply(commands: String*): String = {
-    ???
+    ""
   }
 }
