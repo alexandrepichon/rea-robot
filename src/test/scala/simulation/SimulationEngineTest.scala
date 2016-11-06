6a8e171fbef5cc04d85e1e7a7151e79aa9793bb9
alexandre pichon <alexandre.pichon.ap@gmail.com>
2016-11-06 13:31:41 +0100

    engine: add failing test should ignore MOVE before PLACE

--- src/test/scala/simulation/SimulationEngineTest.scala
+++ src/test/scala/simulation/SimulationEngineTest.scala
@@ -8,15 +8,26 @@ import util.Direction.North
 class SimulationEngineTest extends FunSuite with Matchers {
 
   // Fixtures
-  trait EngineAt00N {
+  trait Engine {
     val engine = new SimulationEngine()
+  }
+
+  trait EngineAt00N extends Engine {
     engine.run(PlaceCommand(0, 0, North))
   }
+
+
 
   // Tests
   test("should ignore the sixth move") {
     new EngineAt00N {
       (1 to 5).foreach(_ => engine.run(MoveCommand))
+      engine.run(MoveCommand).toString should include(" ignored ")
+    }
+  }
+
+  test("should ignore moves before place") {
+    new Engine {
       engine.run(MoveCommand).toString should include(" ignored ")
     }
   }
